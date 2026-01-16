77db860ada57b7a8929402e1be33614dd83fc60e
grop <marcgloredana@gmail.com>
2026-01-16 10:02:32 +0200

    getUser update

--- src/main/java/com/example/demo/api/controller/UserController.java
+++ src/main/java/com/example/demo/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.api.controller;
 import com.example.demo.api.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,8 @@ public class UserController {
         this.userService=userService;
     }
 
-    @GetMapping("/user")
-    public User getUser(@RequestParam Integer id){
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") Integer id){
         Optional user = userService.getUser(id);
 
         if(user.isPresent()){
